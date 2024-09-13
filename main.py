01a73bf35d467866392c0dde7f9d9dd6899275c1
Thassiana Camilia Amorim Muller <62359485+ThassiAmorim@users.noreply.github.com>
2024-09-13 16:02:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from funcoes import create_excel
 
-romaneios_path = "romaneios.pdf" 
-# romaneios_path = "teste.pdf" 
-gerenciais_path = "ConsultasGerenciais.pdf"  
-gabarito_path = "gabarito.xlsx"
-resultados_path = "resultados_atualizados.xlsx"
-
-create_excel(romaneios_path, gerenciais_path, gabarito_path, resultados_path)
+  romaneios_path = "romaneios.pdf" # pdf com os romaneios de entrada
+  gabarito_excel_path = "gabarito.xlsx" # tabela com a primeira coluna de municipios e a segunda coluna com o nome dos estabelecimentos 
+  save_path = "resultados_atualizados.xlsx" # saida atualizada com os dados já extraidos
+  gerenciais_path = "ConsultasGerenciais.pdf" # consultas do sistema web 
+  create_excel(romaneios_path, gerenciais_path, gabarito_excel_path, save_path)
